d84e347b5585262261f0359cc59f24fd478a7405
Simon Mayer <40806645+HabHunger@users.noreply.github.com>
2020-07-15 13:51:18 +0200

    updated entitys

--- app/models.py
+++ app/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    compid=models.UUIDField()
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
 
 class Machine(models.Model):
     machid= models.UUIDField()
     name = models.CharField(max_length=64)
     description= models.CharField(max_length=1024)
     installation_date = models.DateField('date installed')
+    manufacturer = models.ForeignKey(Company, on_delete=models.PROTECT, related_name="manufacturer")
+    operator = models.ForeignKey(Company, on_delete=models.PROTECT, related_name="operator")
 
 
 class Component(models.Model):
@@ -16,6 +22,8 @@ class Component(models.Model):
     wearpart= models.BooleanField()
     sparepart=models.BooleanField()
     cyclecounter = models.IntegerField()
+    maintenanceintervall=models.IntegerField()
+
 
 
 class ComponentGroup(models.Model):
@@ -26,3 +34,4 @@ class ComponentGroup(models.Model):
 class Maintenance(models.Model):
     mainid= models.UUIDField()
     maindate=models.DateTimeField()
+    maindescpription=models.CharField(max_length=256)
--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
 from app import views
+app_name = 'app'
 urlpatterns = [
     # Machine
-    path('machines/', views.Machine.MachineList.as_view(), name='machine_list'),
+    path('machines/', views.MachineList.as_view(), name='machine_list'),
     path('machine/<int:pk>/', views.MachineView.as_view(), name='machine_detail'),
     path('machine/edit/<int:pk>/', views.MachineUpdate.as_view(), name='machine_edit'),
     path('machine/create/', views.MachineCreate.as_view(), name='machine_create'),
