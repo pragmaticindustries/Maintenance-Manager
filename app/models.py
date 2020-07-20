2f286d44b9b26526deca456245facccb401610fd
Simon Mayer <40806645+HabHunger@users.noreply.github.com>
2020-07-20 21:20:37 +0200

    zwischenstand

--- app/models.py
+++ app/models.py
@@ -1,34 +1,52 @@
 from django.db import models
 
+
 # Create your models here.
 class Company(models.Model):
-    compid=models.UUIDField()
+    compid = models.UUIDField()
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
 
-class Machine(models.Model):
-    machid= models.UUIDField()
-    name = models.CharField(max_length=64)
-    description = models.CharField(max_length=1024)
-    installation_date = models.DateField('date installed')
-    manufacturer = models.ForeignKey(Company, on_delete=models.PROTECT, related_name="manufacturer")
-    operator = models.ForeignKey(Company, on_delete=models.PROTECT, related_name="operator")
 
-class Component(models.Model):
-    comid = models.UUIDField()
-    name = models.CharField(max_length=32)
-    description=models.CharField(max_length=256)
-    wearpart= models.BooleanField()
-    sparepart=models.BooleanField()
-    cyclecounter = models.IntegerField()
-    maintenanceintervall=models.IntegerField()
+class Operator(models.Model):
+    opid = models.UUIDField()
+    name = models.CharField(max_length=256)
+
 
 class ComponentGroup(models.Model):
     comgrid = models.UUIDField()
     name = models.CharField(max_length=32)
-    description=models.CharField(max_length=256)
+    description = models.CharField(max_length=256)
+    cyclecounter = models.IntegerField(default=0)
+
+
+class Machine(models.Model):
+    machid = models.UUIDField()
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=1024)
+    installation_date = models.DateField('date installed')
+    manufacturer = models.ForeignKey(Company, on_delete=models.PROTECT, related_name="manufacturer")
+    operator = models.ForeignKey(Operator, on_delete=models.PROTECT, related_name="operator")
+    componentgroup = models.ForeignKey(ComponentGroup, on_delete=models.PROTECT, related_name="machinecomponentgroup")
+
+
+class Component(models.Model):
+    comid = models.UUIDField()
+    name = models.CharField(max_length=32)
+    description = models.CharField(max_length=256)
+    wearpart = models.BooleanField()
+    sparepart = models.BooleanField()
+    cyclecounter = models.IntegerField(default=0)
+    maintenanceintervall = models.IntegerField()
+
 
 class Maintenance(models.Model):
-    mainid= models.UUIDField()
-    maindate=models.DateTimeField()
-    maindescpription=models.CharField(max_length=256)
+    mainid = models.UUIDField()
+    maindate = models.DateTimeField()
+    maindescpription = models.CharField(max_length=256)
+
+
+class PlanedMaintencance(models.Model):
+    plmainid = models.UUIDField()
+    plmaindate = models.DateTimeField()
+    plmaindescription = models.CharField(max_length=256)
